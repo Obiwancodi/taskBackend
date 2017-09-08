e6f6bdfd4a83b6b4f5dc4536969b5bfcc562fba8
Obiwancodi <gflemingiii@gmail.com>
2017-09-08 01:16:32 -0400

    need to make realtionship tables

--- models/_db.js
+++ models/_db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const config = require('../config')
 const db = new Sequelize(config.tableName, config.userName, config.password,{
-    host: 'taskdb.cpw46tuovq7f.us-east-2.rds.amazonaws.com',
+    host: config.host,
     port: '5432',
     dialect: 'postgres',
     protocol: 'postgres',
